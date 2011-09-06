38a70c0d3b97d34a5f36a760549fa814ef0c888e
Martin <martin.laine@gmail.com>
2011-09-06 15:47:01 +0100

    Added current template indicator to toolbar

--- prototype/views.py
+++ prototype/views.py
@@ -7,22 +7,23 @@ from django.template.context import RequestContext
 from django.http import Http404
 
 def list_templates(request):
-	return render(request, 'prototype/index.html')
+	return render(request, 'prototype/index.html', {'current_template': ''})
 
 def show_template(request, page):
 	# Add template tags (so we don't need to load them in each template)
 	add_to_builtins('prototype.template_tags.proto')
+	
 	try:
-		return add_toolbar(render(request, page), request)
+		# Render template
+		response = render(request, page, {'current_template': page})
+		
+		# Render the toolbar
+		toolbar = render_to_string('prototype/toolbar.html', {'current_template': page}, context_instance=RequestContext(request))
+		
+		# Inject the toolbar
+		response.content = re.sub(r'(</(body|BODY)\>)', r'%s\1' % toolbar, response.content.decode(settings.FILE_CHARSET))
+		
+		return response
 	except TemplateDoesNotExist:
 		request.page = page
 		raise Http404
-
-def add_toolbar(response, request):
-	# Render the toolbar
-	toolbar = render_to_string('prototype/toolbar.html', context_instance=RequestContext(request))
-	
-	# Inject the toolbar
-	response.content = re.sub(r'(</(body|BODY)\>)', r'%s\1' % toolbar, response.content.decode(settings.FILE_CHARSET))
-	
-	return response
