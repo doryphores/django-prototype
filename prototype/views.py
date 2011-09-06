632550a0e1daaec4402e091529cd827d6643c3cd
Martin <martin.laine@gmail.com>
2011-09-06 15:06:49 +0100

    Added 404 template

--- prototype/views.py
+++ prototype/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.template import add_to_builtins
+from django.template import add_to_builtins, TemplateDoesNotExist
 from django.template.loader import render_to_string
 import re
 from django.conf import settings
 from django.template.context import RequestContext
+from django.http import Http404
 
 def list_templates(request):
 	return render(request, 'prototype/index.html')
@@ -11,7 +12,11 @@ def list_templates(request):
 def show_template(request, page):
 	# Add template tags (so we don't need to load them in each template)
 	add_to_builtins('prototype.template_tags.proto')
-	return add_toolbar(render(request, page), request)
+	try:
+		return add_toolbar(render(request, page), request)
+	except TemplateDoesNotExist:
+		request.page = page
+		raise Http404
 
 def add_toolbar(response, request):
 	# Render the toolbar
